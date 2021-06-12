e689d3c05e01d4d90d6b6e0d38807866b8b681ae
Karn Kaul <karnkaul@gmail.com>
2021-06-12 11:27:51 -0700

    Remove locker, lockable (refactored and moved to tmutex)

--- async_queue.hpp
+++ async_queue.hpp
@@ -4,8 +4,8 @@
 #pragma once
 #include <condition_variable>
 #include <deque>
+#include <mutex>
 #include <optional>
-#include "lockable.hpp"
 
 namespace kt {
 ///
@@ -73,7 +73,7 @@ class async_queue {
 	///
 	std::condition_variable m_cv;
 	std::deque<T> m_queue;
-	mutable lockable_t<Mutex> m_mutex;
+	mutable Mutex m_mutex;
 	///
 	/// \brief Used to halt / deactivate queue
 	///
@@ -94,7 +94,7 @@ template <typename T, typename Mutex>
 template <typename... U>
 void async_queue<T, Mutex>::emplace(U&&... u) {
 	{
-		auto lock = m_mutex.lock();
+		std::scoped_lock lock(m_mutex);
 		if (m_active) { m_queue.emplace_back(std::forward<U>(u)...); }
 	}
 	m_cv.notify_one();
@@ -104,7 +104,7 @@ template <typename T, typename Mutex>
 template <template <typename...> typename C, typename... Args>
 void async_queue<T, Mutex>::push(C<T, Args...>&& ts) {
 	{
-		auto lock = m_mutex.lock();
+		std::scoped_lock lock(m_mutex);
 		if (m_active) { std::move(ts.begin(), ts.end(), std::back_inserter(m_queue)); }
 	}
 	m_cv.notify_all();
@@ -112,7 +112,7 @@ void async_queue<T, Mutex>::push(C<T, Args...>&& ts) {
 
 template <typename T, typename Mutex>
 std::optional<T> async_queue<T, Mutex>::pop() {
-	auto lock = m_mutex.template lock<std::unique_lock>();
+	std::unique_lock lock(m_mutex);
 	m_cv.wait(lock, [this]() -> bool { return !m_queue.empty() || !m_active; });
 	if (m_active && !m_queue.empty()) {
 		auto ret = std::move(m_queue.front());
@@ -126,7 +126,7 @@ template <typename T, typename Mutex>
 std::deque<T> async_queue<T, Mutex>::clear(bool active) {
 	decltype(m_queue) ret;
 	{
-		auto lock = m_mutex.lock();
+		std::scoped_lock lock(m_mutex);
 		ret = std::move(m_queue);
 		m_queue.clear();
 		m_active = active;
@@ -137,20 +137,20 @@ std::deque<T> async_queue<T, Mutex>::clear(bool active) {
 
 template <typename T, typename Mutex>
 bool async_queue<T, Mutex>::empty() const {
-	auto lock = m_mutex.lock();
+	std::scoped_lock lock(m_mutex);
 	return m_queue.empty();
 }
 
 template <typename T, typename Mutex>
 bool async_queue<T, Mutex>::active() const {
-	auto lock = m_mutex.lock();
+	std::scoped_lock lock(m_mutex);
 	return m_active;
 }
 
 template <typename T, typename Mutex>
 void async_queue<T, Mutex>::active(bool set) {
 	{
-		auto lock = m_mutex.lock();
+		std::scoped_lock lock(m_mutex);
 		m_active = set;
 	}
 	m_cv.notify_all();
